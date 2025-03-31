26a9f0ac056b02aab22d5b6628c4fc872cf0b207
techux <connect.world12345@gmail.com>
2025-03-31 15:49:56 +0530

    Added refresh token field in user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -17,13 +17,16 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: null,
     },
+    refreshToken: {
+      type: String,
+      default: null,
+    },
   },
   {
     timestamps: true,
     versionKey: false,
   }
 );
-5;
 
 userSchema.pre("save", function (next) {
   if (this.accessToken) {
@@ -33,6 +36,16 @@ userSchema.pre("save", function (next) {
     );
     this.accessToken = Buffer.concat([
       cipher.update(this.accessToken),
+      cipher.final(),
+    ]);
+  }
+  if (this.refreshToken) {
+    const cipher = crypto.createCipher(
+      "aes256",
+      process.env.GOOGLE_TOKEN_ENCRYPT_KEY
+    );
+    this.refreshToken = Buffer.concat([
+      cipher.update(this.refreshToken),
       cipher.final(),
     ]);
   }
@@ -48,6 +61,16 @@ userSchema.post("find", function (docs) {
       );
       doc.accessToken = Buffer.concat([
         decipher.update(doc.accessToken),
+        decipher.final(),
+      ]).toString();
+    }
+    if (doc.refreshToken) {
+      const decipher = crypto.createDecipher(
+        "aes256",
+        process.env.GOOGLE_TOKEN_ENCRYPT_KEY
+      );
+      doc.refreshToken = Buffer.concat([
+        decipher.update(doc.refreshToken),
         decipher.final(),
       ]).toString();
     }
