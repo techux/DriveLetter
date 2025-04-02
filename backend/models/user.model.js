c53f3ba7df4a61084cfeb9036e53a27a8b6cb5aa
techux <connect.world12345@gmail.com>
2025-04-03 02:54:09 +0530

    Removed: Google token encryption from user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -28,54 +28,6 @@ const userSchema = new mongoose.Schema(
   }
 );
 
-userSchema.pre("save", function (next) {
-  if (this.accessToken) {
-    const cipher = crypto.createCipher(
-      "aes256",
-      process.env.GOOGLE_TOKEN_ENCRYPT_KEY
-    );
-    this.accessToken = Buffer.concat([
-      cipher.update(this.accessToken),
-      cipher.final(),
-    ]);
-  }
-  if (this.refreshToken) {
-    const cipher = crypto.createCipher(
-      "aes256",
-      process.env.GOOGLE_TOKEN_ENCRYPT_KEY
-    );
-    this.refreshToken = Buffer.concat([
-      cipher.update(this.refreshToken),
-      cipher.final(),
-    ]);
-  }
-  next();
-});
-
-userSchema.post("find", function (docs) {
-  docs.forEach((doc) => {
-    if (doc.accessToken) {
-      const decipher = crypto.createDecipher(
-        "aes256",
-        process.env.GOOGLE_TOKEN_ENCRYPT_KEY
-      );
-      doc.accessToken = Buffer.concat([
-        decipher.update(doc.accessToken),
-        decipher.final(),
-      ]).toString();
-    }
-    if (doc.refreshToken) {
-      const decipher = crypto.createDecipher(
-        "aes256",
-        process.env.GOOGLE_TOKEN_ENCRYPT_KEY
-      );
-      doc.refreshToken = Buffer.concat([
-        decipher.update(doc.refreshToken),
-        decipher.final(),
-      ]).toString();
-    }
-  });
-});
 
 const User = mongoose.model("user", userSchema);
 
