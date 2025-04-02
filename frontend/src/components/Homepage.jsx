d6a2746df3c3e836169c0960d89679b3aac213c7
techux <connect.world12345@gmail.com>
2025-04-03 03:32:20 +0530

    Added: Feature to Save file to Drive, Integrated HTML format Editor, Option to add filename

--- frontend/src/components/Homepage.jsx
+++ frontend/src/components/Homepage.jsx
@@ -4,7 +4,9 @@ import Button from "./Button";
 import { Link } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 
-const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+import Editor from "react-simple-wysiwyg";
+
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 const isTokenExpired = (token) => {
   try {
@@ -19,6 +21,7 @@ const isTokenExpired = (token) => {
 const Homepage = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [letter, setLetter] = useState("");
+  const [filename, setFilename] = useState(`DriveLetter_${Date.now()}`);
   const [loading, setLoading] = useState(false);
   const [message, setMessage] = useState("");
   const [error, setError] = useState("");
@@ -37,9 +40,71 @@ const Homepage = () => {
     setLetter(e.target.value);
   };
 
+  const handleFilenameChange = (e) => {
+    setFilename(e.target.value);
+  };
+
   const handleSaveDraft = async () => {
     setMessage("");
     setError("");
+
+    if (!letter.trim()) {
+      toast.warn("Letter cannot be empty");
+      setError("Letter cannot be empty.");
+      return;
+    }
+
+    if (!letter.trim()) {
+      setFilename(`DriveLetter_${Date.now()}`);
+    }
+
+    setLoading(true);
+    setError("");
+
+    try {
+      const accessToken = localStorage.getItem("accessToken");
+      if (!accessToken) {
+        setError("Authentication required.");
+        setLoading(false);
+        return;
+      }
+
+      const response = await fetch(`${API_BASE_URL}/letter/draft`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({
+          content: letter,
+          filename: filename,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        toast.success("Draft saved successfully!");
+        setMessage("Draft saved successfully.");
+        setLetter("");
+        setFilename("");
+      } else {
+        toast.error(data.error || "Failed to save draft.");
+        setError(data.error || "Failed to save draft.");
+      }
+    } catch (error) {
+      toast.error("An error occurred while saving");
+      setError("An error occurred while saving");
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleSaveToDrive = async () => {
+    setMessage("");
+    setError("");
+
     if (!letter.trim()) {
       toast.warn("Letter cannot be empty");
       setError("Letter cannot be empty.");
@@ -57,7 +122,7 @@ const Homepage = () => {
         return;
       }
 
-      const response = await fetch(`${BASE_URL}/letter/draft`, {
+      const response = await fetch(`${API_BASE_URL}/letter/save`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -65,23 +130,25 @@ const Homepage = () => {
         },
         body: JSON.stringify({
           content: letter,
+          filename: filename,
         }),
       });
 
       const data = await response.json();
 
       if (response.ok) {
-        toast.success("Draft saved successfully!");
-        setMessage("Draft saved successfully.");
+        toast.success("Letter saved successfully!");
+        setMessage("Letter saved successfully.");
         setLetter("");
+        setFilename("");
       } else {
-        toast.error(data.error || "Failed to save draft.");
-        setError(data.error || "Failed to save draft.");
+        toast.error(data.message || "Failed to save on Drive.");
+        setError(data.message || "Failed to on Drive.");
       }
     } catch (error) {
       toast.error("An error occurred while saving");
       setError("An error occurred while saving");
-      // alert(error.message);
+      console.error(error);
     } finally {
       setLoading(false);
     }
@@ -90,7 +157,7 @@ const Homepage = () => {
   if (!isAuthenticated) {
     return (
       <div className="flex justify-center items-center h-screen">
-        <Link to={`${BASE_URL}/auth/google`}>
+        <Link to={`${API_BASE_URL}/auth/google`}>
           <Button text="Login to save Letters" color="blue" />
         </Link>
       </div>
@@ -114,13 +181,32 @@ const Homepage = () => {
 
           {/* Textarea */}
           <div className="mt-4">
-            <textarea
+            {/* <textarea
               value={letter}
               onChange={handleLetterChange}
               className="w-full p-5 bg-slate-600 border-slate-500 border text-white rounded-md resize-none outline-none"
               placeholder="Draft your letter here..."
               rows={20}
+            /> */}
+
+            <div className="mb-3">
+              <label htmlFor="filename">Enter Filename </label>
+              <input
+                type="text"
+                id="filename"
+                value={filename}
+                onChange={handleFilenameChange}
+                placeholder="Enter filename to save..."
+                className="w-64 p-1 bg-slate-600 border-slate-500 border text-white rounded-md resize-none outline-none"
+              />
+            </div>
+
+            <Editor
+              value={letter}
+              onChange={handleLetterChange}
+              className="w-full h-96 p-5 bg-slate-600 border-slate-500"
             />
+
             <div className="flex justify-center gap-3 mt-5">
               <button
                 className="px-4 py-2 rounded text-white bg-yellow-500 hover:bg-yellow-600 focus:outline-none"
@@ -132,9 +218,9 @@ const Homepage = () => {
 
               <button
                 className="px-4 py-2 rounded text-white bg-green-500 hover:bg-green-600 focus:outline-none"
-                onClick={handleSaveDraft}
+                onClick={handleSaveToDrive}
               >
-                Save
+                Save to Drive
               </button>
             </div>
             {message && (
